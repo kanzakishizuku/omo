9271e8c4be8a59eff9cabc9127c358e6b82b1f93
supmiku39 <31564253+supmiku39@users.noreply.github.com>
2017-09-24 21:04:22 +0800

    Update OwO.cpp

--- OwO.cpp
+++ OwO.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+void solve(double a, double b, double c, double *x1, double *x2)
+{
+	*x1 = (-b-sqrt(b*b-4*a*c))/2/a;
+	*x2 = (-b+sqrt(b*b-4*a*c))/2/a;
+}
+
 int main(int argc, char** argv) 
 {
 	double a, b, c1, c2, sol1x, sol1y, sol2x, sol2y;
@@ -15,12 +21,12 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		c1=(-2*b+sqrt(32*a*a+b*b))/(2*b*b+8*a*a);
-		c2=(-2*b-sqrt(32*a*a+b*b))/(2*b*b+8*a*a);
+		double k = b/a;
+		solve(k*k+4, 2*k, -2, &c1, &c2); //(k^2+4)*y^2 + 2ky - 2 = 0
 		sol1x=b*c1+1;
 		sol1y=a*c1;
 		sol2x=b*c2+1;
-		sol2y=a*c2;	
+		sol2y=a*c2;
 	}
 	printf("兩個交點分別為:(%lf,%lf),(%lf,%lf)\n", sol1x, sol1y, sol2x, sol2y);
 	return 0;
